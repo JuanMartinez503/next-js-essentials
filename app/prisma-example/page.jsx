66988ed28b0faf001bf7170e86cea9bc78447c09
Juan Martinez <juanmartinez@Juans-iMac.local>
2024-01-05 12:06:41 -0500

    I added prisma cruds to the project

--- app/prisma-example/page.jsx
+++ app/prisma-example/page.jsx
@@ -1,9 +1,26 @@
+import prisma from "../../utils/db";
 
-export default function PrismaPage (){
+const prismaHandlers = async () => {
+  await prisma.task.create({
+    data: {
+      content: "Hello this is Juan",
+    },
+  });
+  const getAllTask = prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return getAllTask;
+};
 
-    return (
-        <div>
-            PrismaPage
-        </div>
-    )
+export default async function PrismaPage() {
+
+    const tasks = await prismaHandlers()
+
+  return <div>
+    {tasks.map(task=>(
+        <h1 key={task.id} className="text-3xl py-4 ">{task.content}</h1>
+    ))}
+  </div>;
 }
